b6efdb885336355b7a2fe931f5259e537830ebad
ikerorozco <99369597+ikerorozco@users.noreply.github.com>
2025-03-02 16:53:01 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
+master_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'default'
 
